c89b83600cbaaa8a5057f5d386ef39d6cf2484ed
Home <Home@CCH-CK69136-PC.cchpdc.cch.edu.pk>
2026-03-17 21:32:21 +0500

    Update logo and fix sections

--- components/team.tsx
+++ components/team.tsx
@@ -6,8 +6,8 @@ export default function Team() {
       <h3>Azlan Khan</h3>
       <p>Founder, CEO · Backend, AI, Systems</p>
 
-      <h3>Abdul Hadi</h3>
-      <p>COO · Marketing · Design · Visual Systems</p>
+      <h3>Ishel Fatima</h3>
+      <p>CFO · Marketing · Socail Media Management · Branding</p>
 
       <h3>Muhammad Talha</h3>
       <p>CFO · Finance Management · Accounts · Risk Management</p>
